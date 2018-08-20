6d4c7ec65587dc02596d44cd34b9c138c308da1c
Erik Doernenburg <erik@doernenburg.com>
2018-08-20 12:07:05 +0200

    Small refactorings, mostly renames.

--- Patterns.cpp
+++ Patterns.cpp
@@ -21,27 +21,27 @@ uint32_t rgb_g(uint8_t r, uint8_t g, uint8_t b) {
   return frame.Color(frame.gamma8(r), frame.gamma8(g), frame.gamma8(b));
 }
 
-uint32_t mixChannel(uint32_t c1, uint32_t c2, uint16_t p) {
-  return (c1 > c2) ? (c1 - (c1-c2)*p/1000) 
-                   : (c2 - (c2-c1)*(1000-p)/1000);
+uint32_t mixChannel(uint32_t c1, uint32_t c2, uint16_t r) {
+  return (c1 > c2) ? (c1 - (c1-c2)*r/1000) 
+                   : (c2 - (c2-c1)*(1000-r)/1000);
 }
 
-uint32_t mix(uint32_t c1, uint32_t c2, uint16_t p) {
-  return rgb(mixChannel(R(c1), R(c2), p), 
-             mixChannel(G(c1), G(c2), p), 
-             mixChannel(B(c1), B(c2), p));
+uint32_t mix(uint32_t c1, uint32_t c2, uint16_t r) {
+  return rgb(mixChannel(R(c1), R(c2), r), 
+             mixChannel(G(c1), G(c2), r), 
+             mixChannel(B(c1), B(c2), r));
 }
 
-uint32_t mix_g(uint32_t c1, uint32_t c2, uint16_t p) {
-  return rgb_g(mixChannel(R(c1), R(c2), p), 
-               mixChannel(G(c1), G(c2), p), 
-               mixChannel(B(c1), B(c2), p));
+uint32_t mix_g(uint32_t c1, uint32_t c2, uint16_t r) {
+  return rgb_g(mixChannel(R(c1), R(c2), r), 
+               mixChannel(G(c1), G(c2), r), 
+               mixChannel(B(c1), B(c2), r));
 }
 
-uint8_t pidx(int i, uint8_t o, uint8_t dir) {
+uint8_t shift(int idx, uint8_t val, uint8_t dir) {
   uint16_t n = frame.numPixels();
-  uint8_t pidx = (i+o) % n;
-  return (dir == CW) ? pidx : n - pidx;
+  uint8_t result = (idx+val) % n;
+  return (dir == CW) ? result : n - result;
 }
 
 
@@ -49,11 +49,10 @@ uint8_t pidx(int i, uint8_t o, uint8_t dir) {
 
 // Fills LEDs with the given colour
 // c: colour
-// s: spacing between LED to be lit (should be a divisor of 30)
-void fill(uint32_t c, uint8_t s, uint16_t wait) {
+void fill(uint32_t c, uint16_t wait) {
   uint16_t n = frame.numPixels();
   for (int i = 0; i < n; i++) {
-    frame.setPixelColor(i, (i%s) ? rgb(0, 0, 0) : c);
+    frame.setPixelColor(i, c);
   }
   frame.show();
   delay(wait);
@@ -63,16 +62,16 @@ void fill(uint32_t c, uint8_t s, uint16_t wait) {
 // c1: blade colour
 // c2: background colour
 // s: spacing between blades (should be a divisor of 30)
-// w: width of the blads (should be less than s)
+// w: width of the blades (should be less than s)
 // d: direction (1 = clockwise, -1 = counter-clockwise)
 void rotor(uint32_t c1, uint32_t c2, uint8_t s, uint8_t w, int8_t d, uint16_t wait) {
   uint16_t n = frame.numPixels();
   static uint8_t fc = 0;
   fc = (fc+1) % s;
 
-  for (int i = s; i < n+s; i += s) { // extra s because we may subtract fc
+  for (int i = 0; i < n; i += s) {
     for (int j = 0; j < w; j++) {
-      frame.setPixelColor((i+j+d*fc) % n, c1);
+      frame.setPixelColor(shift(i+j, fc, d), c1);
     }
   }
 
@@ -81,7 +80,7 @@ void rotor(uint32_t c1, uint32_t c2, uint8_t s, uint8_t w, int8_t d, uint16_t wa
 
   for (int i = s; i < n+s; i += s) {
     for (int j = 0; j < w; j++) {
-      frame.setPixelColor((i+j+d*fc) % n, c2);
+      frame.setPixelColor(shift(i+j, fc, d), c2);
     }
   }
 }
@@ -93,12 +92,12 @@ void rotor(uint32_t c1, uint32_t c2, uint8_t s, uint8_t w, int8_t d, uint16_t wa
 // dir: direction
 void gradient(uint32_t c1, uint32_t c2, uint8_t s, uint8_t dir, uint16_t wait) {
   uint16_t n = frame.numPixels();
-  static uint8_t fc = 0;
-  fc = (fc+1) % n;
+  static uint8_t offset = 0;
+  offset = (offset+1) % n;
 
   for (int i = 0; i < n; i++) {
     uint16_t r = 1000 - 1000*(i%s)/s;
-    frame.setPixelColor(pidx(i, fc, dir), mix_g(c1, c2, r));
+    frame.setPixelColor(shift(i, offset, dir), mix_g(c1, c2, r));
   }
   
   frame.show();
@@ -110,14 +109,14 @@ void gradient(uint32_t c1, uint32_t c2, uint8_t s, uint8_t dir, uint16_t wait) {
 // c2: second colour
 void doubleGradient(uint32_t c1, uint32_t c2, uint16_t wait) {
   uint16_t n = frame.numPixels();
-  static uint8_t fc = 0;
-  fc = (fc+1) % n;
+  static uint8_t offset = 0;
+  offset = (offset+1) % n;
   
   for (int i = 0; i < n; i++) {
     if (i < n/2) {
-      frame.setPixelColor((i+fc) % n, mix(c1, c2, 1000*2*i/n));
+      frame.setPixelColor(shift(i, offset, CW), mix(c1, c2, 1000*2*i/n));
     } else {
-      frame.setPixelColor((i+fc) % n, mix(c2, c1, 1000*(2*i-n)/n));
+      frame.setPixelColor(shift(i, offset, CW), mix(c2, c1, 1000*(2*i-n)/n));
     }
   }
   
@@ -131,16 +130,16 @@ void doubleGradient(uint32_t c1, uint32_t c2, uint16_t wait) {
 // c3: third colour
 void tripleGradient(uint32_t c1, uint32_t c2, uint32_t c3, uint16_t wait) {
   uint16_t n = frame.numPixels();
-  static uint8_t fc = 0;
-  fc = (fc+1) % n;
+  static uint8_t offset = 0;
+  offset = (offset+1) % n;
   
   for (int i = 0; i < n; i++) {
     if (i < n/3) {
-      frame.setPixelColor((i+fc) % n, mix(c1, c2, 1000*3*i/n));
+      frame.setPixelColor(shift(i, offset, CW), mix(c1, c2, 1000*3*i/n));
     } else if (i < n*2/3) {
-      frame.setPixelColor((i+fc) % n, mix(c2, c3, 1000*(3*i-n)/n));
+      frame.setPixelColor(shift(i, offset, CW), mix(c2, c3, 1000*(3*i-n)/n));
     } else {
-      frame.setPixelColor((i+fc) % n, mix(c3, c1, 1000*(3*i-2*n)/n));
+      frame.setPixelColor(shift(i, offset, CW), mix(c3, c1, 1000*(3*i-2*n)/n));
     }
   }
   
@@ -174,13 +173,13 @@ void lightning(uint32_t c1, uint32_t c2, uint8_t w, uint16_t wait) {
 
 void breathe() 
 {
-  static uint8_t fc = 0;
-  fc = (fc+1) % 256;
+  static uint8_t counter = 0;
+  counter = (counter+1) % 256;
 
-  uint8_t x = frame.sine8(fc);
+  uint8_t x = frame.sine8(counter);
   uint8_t l = frame.gamma8(x/2+8); // shifting by 8 to get roughly 57 zeroes in a cycle
   // breatheDiagnostics(l);
-  fill(rgb(0, 0, l), 1, 9);
+  fill(rgb(0, 0, l), 9);
 }
 
 void breatheDiagnostics(uint8_t l) {
@@ -209,27 +208,27 @@ void breatheDiagnostics(uint8_t l) {
 // OTHER PATTERNS
 
 void clear() {
-  fill(rgb(0, 0, 0), 1, 500);
+  fill(rgb(0, 0, 0), 500);
 }
 
 void fillDarkBlue() {
-  fill(rgb(0, 0, 49), 1, 500);
+  fill(rgb(0, 0, 49), 500);
 }
 
 void blueFiveBladeRotor() {
-  rotor(rgb_g(0, 0, 255), rgb_g(0, 0, 96), 6, 1, -1, 50);
+  rotor(rgb_g(0, 0, 255), rgb_g(0, 0, 96), 6, 1, CCW, 50);
 }
 
 void magentaSpinner() {
-  rotor(rgb(255, 0, 255), rgb(0, 0, 150), 30, 4, 1, 30);
+  rotor(rgb(255, 0, 255), rgb(0, 0, 150), 30, 4, CW, 30);
 }
 
 void redPropeller() {
-  rotor(rgb(150, 0, 0), rgb(0, 0, 100), 15, 3, 1, 40);
+  rotor(rgb(150, 0, 0), rgb(0, 0, 100), 15, 3, CW, 40);
 }
 
 void clockHand() {
-  rotor(rgb(255, 0, 0), rgb(0, 0, 150), 30, 2, 1, 1000);
+  rotor(rgb(255, 0, 0), rgb(0, 0, 150), 30, 2, CW, 1000);
 }
 
 void darkOrangeSweep() {
